2d9e1befae4261999320b242c5f7e4556994520f
CrispthoAlex <alex877@gmail.com>
2020-12-10 19:21:14 -0500

    Add more text to welcome-text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hi everyone,<br></br>
+          Welcome to React Course.<br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/crispthoalex"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my GitHub
         </a>
       </header>
     </div>
